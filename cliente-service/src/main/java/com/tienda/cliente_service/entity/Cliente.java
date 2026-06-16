9b2e73833ce22fd988f04dd83dabfc98550baa12
AV-Alumno <AV-Alumno@duoc.acad>
2026-06-16 16:44:02 -0400

    fix: corregir poms para compatibilidad Lombok con Java 24

--- cliente-service/src/main/java/com/tienda/cliente_service/entity/Cliente.java
+++ cliente-service/src/main/java/com/tienda/cliente_service/entity/Cliente.java
@@ -2,26 +2,57 @@ package com.tienda.cliente_service.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.Data;
 
 @Entity
 @Table(name = "clientes")
-@Data
 public class Cliente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "El nombre es obligatorio")
-    @Size(min = 3 , message = "minimo requerido de 3 caracteres")
+    @Size(min = 3, message = "minimo requerido de 3 caracteres")
     private String nombre;
-    
+
     @NotBlank
     @Size(min = 11, message = "el numero debe tener 8 digitos, más los digitos de zona (569)")
     private String telefono;
 
     @Email(message = "El formato del email no es válido")
-    @Size(min = 10, message =  " minimo requerido de 10 caracteres")
+    @Size(min = 10, message = "minimo requerido de 10 caracteres")
     @NotBlank(message = "El email es obligatorio")
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
