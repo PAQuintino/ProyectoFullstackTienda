b8c13e52516bb129d87d7c4d74f8dea5912c3d96
Pablo Quintino <pa.quintino@duocuc.cl>
2026-06-18 17:13:34 -0400

    arreglado

---
+++ cliente-service/src/main/java/com/tienda/cliente_service/security/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.tienda.cliente_service.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public boolean validarToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(getKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extraerEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    private SecretKey getKey() {
+        byte[] bytes = Decoders.BASE64.decode(Encoders.BASE64.encode(secret.getBytes()));
+        return Keys.hmacShaKeyFor(bytes);
+    }
+}
